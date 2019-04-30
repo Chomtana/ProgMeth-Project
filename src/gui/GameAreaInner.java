7e1e142359ceb771c8fca8d24f3b0f2728677979
Pongsakorn <pongsakorn_pong@hotmail.co.th>
2019-04-30 15:21:56 +0700

    new : Generating Ore

--- src/gui/GameAreaInner.java
+++ src/gui/GameAreaInner.java
@@ -11,71 +11,161 @@ import entity.monster.Zombie;
 import javafx.application.Platform;
 import javafx.scene.layout.GridPane;
 
-public class GameAreaInner extends GridPane {
-	
+public class GameAreaInner extends GridPane
+{
+
 	public static int NUM_COL = 50;
 	public static int NUM_ROW = 50;
-	private HashMap<Integer, HashMap<Integer, Block>> blocks = new HashMap<Integer, HashMap<Integer,Block>>();
-	
-	public GameAreaInner() {
+	private HashMap<Integer, HashMap<Integer, Block>> blocks = new HashMap<Integer, HashMap<Integer, Block>>();
+	private boolean isDiamondOre[][] = new boolean[NUM_ROW][NUM_COL];
+	private boolean isIronOre[][] = new boolean[NUM_ROW][NUM_COL];
+	private boolean isCoalOre[][] = new boolean[NUM_ROW][NUM_COL];
+	private int dirI[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+	private int dirJ[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+	public GameAreaInner()
+	{
 		super();
-		//Block b1 = new Block();
-		
-		for(int i = 0;i<NUM_COL;i++) {
-			for (int j = 0;j<NUM_ROW;j++) {
-				Block block = new Block(j,i);
-				if (i==0 && j==0) {
-					
+		// Block b1 = new Block();
+
+		for (int i = 0; i < NUM_COL; i++)
+		{
+			for (int j = 0; j < NUM_ROW; j++)
+			{
+				Block block = new Block(j, i);
+				if (i == 0 && j == 0)
+				{
+
 					block.setEntity(Player.mainPlayer);
 				}
-				if (i==5 && j==5) {
-					block.setEntity(new Zombie(j,i));
+				if (i == 5 && j == 5)
+				{
+					block.setEntity(new Zombie(j, i));
 				}
-				if (!blocks.containsKey(j)) blocks.put(j, new HashMap());
+				if (!blocks.containsKey(j))
+					blocks.put(j, new HashMap());
 				blocks.get(j).put(i, block);
 				this.add(block, i, j);
 			}
 		}
-		
-		Platform.runLater(new Runnable() {
-			
+
+		Platform.runLater(new Runnable()
+		{
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				Player.mainPlayer.moveDown();
-				
+
 			}
 		});
-		
-		new TestEffect(0,0);
 
-		EventController.onLoad(new Runnable() {
-			
+		new TestEffect(0, 0);
+
+		EventController.onLoad(new Runnable()
+		{
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				
-				for(int i = 0;i<NUM_ROW;i++) {
-					for (int j = 0;j<NUM_COL;j++) {
-						if (!Block.getBlock(i, j).hasEntity()) {
-							if (Main.random.nextInt(100) < 50) {
-								new Zombie(i,j);
-							}
+				for (int i = 0; i < NUM_ROW; i++)
+					for (int j = 0; j < NUM_COL; j++)
+						isDiamondOre[i][j] = isIronOre[i][j] = isCoalOre[i][j] = false;
+
+				// Gen Diamond Ore
+				for (int i = 0; i < NUM_ROW; i += 25)
+				{
+					for (int j = 0; j < NUM_COL; j += 25)
+					{
+						int nowRow = Main.random.nextInt(25) + i;
+						int nowCol = Main.random.nextInt(25) + j;
+						isDiamondOre[nowRow][nowCol] = true;
+						
+						for (int k = 0; k < 8; k++)
+						{
+							int nextRow = nowRow + dirI[k];
+							int nextCol = nowCol + dirJ[k];
+							if (nextRow < 0 || nextCol < 0 || nextRow >= NUM_ROW || nextCol >= nextCol)
+								continue;
+							if (Main.random.nextInt(2) == 1)
+								isDiamondOre[nextRow][nextCol] = true;
+							else
+								isDiamondOre[nextRow][nextCol] |= false;
 						}
 					}
 				}
-				
-				
+
+				// Gen Iron Ore
+				for (int i = 0; i < NUM_ROW; i += 20)
+				{
+					for (int j = 0; j < NUM_COL; j += 20)
+					{
+						int nowRow = Main.random.nextInt(20) + i;
+						int nowCol = Main.random.nextInt(20) + j;
+						isIronOre[nowRow][nowCol] = true;
+						
+						int temp = Main.random.nextInt(8);
+						int tempRow = nowRow + dirI[temp];
+						int tempCol = nowCol + dirJ[temp];
+						isIronOre[tempRow][tempCol] = true;
+						
+						for (int k = 0; k < 8; k++)
+						{
+							int nextRow = nowRow + dirI[k];
+							int nextCol = nowCol + dirJ[k];
+							if (nextRow < 0 || nextCol < 0 || nextRow >= NUM_ROW || nextCol >= nextCol)
+								continue;
+							if (Main.random.nextInt(2) == 1)
+								isIronOre[nextRow][nextCol] = true;
+							else
+								isIronOre[nextRow][nextCol] |= false;
+						}
+						
+						for (int k = 0; k < 8; k++)
+						{
+							int nextRow = tempRow + dirI[k];
+							int nextCol = tempCol + dirJ[k];
+							if (nextRow < 0 || nextCol < 0 || nextRow >= NUM_ROW || nextCol >= nextCol)
+								continue;
+							if (Main.random.nextInt(2) == 1)
+								isIronOre[nextRow][nextCol] = true;
+							else
+								isIronOre[nextRow][nextCol] |= false;
+						}
+					}
+				}
+
+				// Gen Coal Ore
+				for (int i = 0; i < NUM_ROW; i += 10)
+				{
+					for (int j = 0; j < NUM_COL; j += 10)
+					{
+						int nowRow = Main.random.nextInt(10) + i;
+						int nowCol = Main.random.nextInt(10) + j;
+						isDiamondOre[nowRow][nowCol] = true;
+						
+						for (int k = 0; k < 8; k++)
+						{
+							int nextRow = nowRow + dirI[k];
+							int nextCol = nowCol + dirJ[k];
+							if (nextRow < 0 || nextCol < 0 || nextRow >= NUM_ROW || nextCol >= nextCol)
+								continue;
+							if (Main.random.nextInt(2) == 1)
+								isDiamondOre[nextRow][nextCol] = true;
+							else
+								isDiamondOre[nextRow][nextCol] |= false;
+						}
+					}
+				}
+
 			}
 		});
 
-		
-		//this.setPrefWidth(270);
+		// this.setPrefWidth(270);
 	}
 
 	public HashMap<Integer, HashMap<Integer, Block>> getBlocks() {
 		return blocks;
 	}
-	
-	
+
 }
